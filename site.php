6cbfc2a3f00edfc3871813b283bd704e717f5aa3
hek <yeyu0424@163.com>
2020-05-15 11:50:05 +0800

    添加项目文件

---
+++ site.php
@@ -0,0 +1,22 @@
+<?php
+
+global $_W,$_GPC;
+defined('IN_IA') or exit('Access Denied');
+define('ST_ROOT',IA_ROOT.'/addons/zofui_sitetemp/');
+define('ST_URL',$_W['siteroot'].'addons/zofui_sitetemp/');
+define('MODULE','zofui_sitetemp');
+require_once(ST_ROOT.'class/autoload.php');
+
+class Zofui_sitetempModuleSite extends plugin {
+	
+	
+	function __call($name, $arguments){
+		global $_GPC,$_W;
+		$_W['set'] = $this->module['config'];
+		
+		$this->pInit( $_GPC['p'] ) || parent::__call($name, $arguments);
+	}
+	
+	
+	
+}
